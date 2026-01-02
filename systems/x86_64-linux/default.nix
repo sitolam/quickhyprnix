670828e453787da2625cc679a6499a1b4a685076
sitolam <79202140+sitolam@users.noreply.github.com>
2026-01-03 00:08:40 +0100

    fix: update to 25.11

--- systems/x86_64-linux/default.nix
+++ systems/x86_64-linux/default.nix
@@ -188,6 +188,11 @@
     };
   };
 
+  environment.pathsToLink = [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
