f71a113e3c73517e440e46485b236b8ad3aeccbd
sitolam <79202140+sitolam@users.noreply.github.com>
2026-01-04 11:32:20 +0100

    feat: add update script

--- modules/nh/default.nix
+++ modules/nh/default.nix
@@ -3,11 +3,25 @@
   config,
   lib,
   pkgs,
+  username,
   ...
 }:
 
 let
   cfg = config.system.nh;
+
+  update = pkgs.writeShellScriptBin "update" ''
+    env NH_FLAKE="$NH_FLAKE"
+    clear
+    nh os switch --impure
+    if [ $? -ne 0 ]; then
+        notify-send "Error" "Failed to switch NixOS configuration."
+    else
+        reload-noctalia
+        notify-send -u low "Success" "NixOS configuration switched successfully."
+    fi
+    read -n 1 -p 'Press any key to continue...'
+  '';
 in
 {
   options.system.nh = {
@@ -16,14 +30,17 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # home.extraOptions = {
-    programs.nh = {
-      enable = true;
-      flake = "/home/otis/quickhyprnix"; # TODO make dynamic
-      # NOTE  set location of flake config (so you don't have to type it every time you rebuild)
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 30d --keep 5"; # TODO nog eens bekijken
+    home.extraOptions = {
+      programs.nh = {
+        enable = true;
+        flake = "/home/${username}/quickhyprnix"; # TODO make dynamic
+        # NOTE  set location of flake config (so you don't have to type it every time you rebuild)
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 30d --keep 5"; # TODO nog eens bekijken
+      };
     };
-    # };
+    home.extraOptions.home.packages = with pkgs; [
+      update
+    ];
   };
 }
