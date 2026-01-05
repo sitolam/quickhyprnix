8f8628248247943bd084e1e78165667f632ed32c
sitolam <79202140+sitolam@users.noreply.github.com>
2026-01-05 20:16:21 +0100

    fix: kde-connect open ports

--- modules/services/kde-connect.nix
+++ modules/services/kde-connect.nix
@@ -24,5 +24,19 @@ in
     home.extraOptions.services.kdeconnect = {
       enable = true;
     };
+    networking.firewall = {
+      allowedTCPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        }
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        }
+      ];
+    };
   };
 }
