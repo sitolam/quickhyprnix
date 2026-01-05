661de5ec3210c42b18e0c61a8fd619fe4819289f
sitolam <79202140+sitolam@users.noreply.github.com>
2026-01-05 20:44:51 +0100

    chore: ideas for anki

--- modules/apps/anki/default.nix
+++ modules/apps/anki/default.nix
@@ -16,10 +16,11 @@ in
 
   config = lib.mkIf cfg.enable {
     home.extraOptions.home.packages = with pkgs; [ anki-bin ];
+    #TODO maybe with this option and addons here (not with syncthing), maybe disable stylix theming but custom theming with recolor
+
     # home.extraOptions.programs.anki = {
     #   enable = true;
     # };
-    # TODO add add-ons: https://www.phind.com/search?cache=j2jdufv9f9lrr1dnf6k54vbw
-    # TODO other way to add add-ons?: https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/misc/markdown-anki-decks/default.nix
+
   };
 }
