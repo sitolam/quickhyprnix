a01128d3e29b770754519a72c01a6f412d198d37
sitolam <79202140+sitolam@users.noreply.github.com>
2026-01-05 20:01:34 +0100

    feat: add auto start to social apps

--- modules/desktop/niri/autostart.nix
+++ modules/desktop/niri/autostart.nix
@@ -17,6 +17,20 @@
     { command = [ "noctalia-shell" ]; }
     { command = [ "vesktop" ]; }
     { command = [ "swww-daemon" ]; }
+    { command = [ "kdeconnectd" ]; }
+    { command = [ "discord --start-minimized" ]; }
+    {
+      command = [
+        "signal-desktop"
+        "--start-in-tray"
+      ];
+    }
+    {
+      command = [
+        "element-desktop"
+        "--hidden"
+      ];
+    }
     #{ command = ["${pkgs.swaybg}/bin/swaybg" "-o" "DP-1" "-i" "/home/chris/nixos/assets/wallpapers/clouds.png" "-m" "fill"]; }
     #{ command = ["sh" "-c" "swww-daemon & swww img /home/chris/nixos/wallpapers/cloud.png"]; }
   ];
